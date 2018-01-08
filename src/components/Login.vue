4518c9cf53b931ab538457da6c67a10a4b202a2e
Allan Demarbre <demarbreallan@gmail.com>
2018-01-08 15:33:59 +0100

    Ajout d'une route de connexion

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <form action="" method="post">
+            <input type="text" name="" id="" placeholder="email">
+            <input type="text" name="" id="" placeholder="password">
+            <input type="button" value="Login">
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
