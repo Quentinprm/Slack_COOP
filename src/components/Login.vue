b1f8605e75da2a4df87c2bdce5dcc0b8b47a4086
Allan Demarbre <demarbreallan@gmail.com>
2018-01-08 16:37:53 +0100

    Commit before merge

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,34 @@
 <template>
     <div>
         <form action="" method="post">
-            <input type="text" name="" id="" placeholder="email">
-            <input type="text" name="" id="" placeholder="password">
-            <input type="button" value="Login">
+            <input v-model="user.email" type="text" name="" id="email" placeholder="email">
+            <input v-model="user.password" type="password" name="" id="password" placeholder="password">
+            <input type="button" value="Login" @click="login">
         </form>
     </div>
 </template>
 
 <script>
-    export default {}
+    import api from '../api'
+
+    export default {
+        data() {
+            return {
+                user: {
+                    email: '', 
+                    password: ''
+                }
+            }
+        },
+        methods: {
+            login () {
+                api.post("/members/signin", this.user).then( (res) => {
+                    console.log(res.data.token)
+                    this.$store.dispatch('login', res.data.token)
+                });
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
