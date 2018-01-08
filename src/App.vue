8aa9bc5e4995914141f0fbf031c716d1e93dfba0
Quentin.prm <quentin.prm@gmail.com>
2018-01-08 16:33:56 +0100

    logo and longin template update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.svg">
     <router-view/>
   </div>
 </template>
@@ -13,11 +13,14 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Tahoma, Geneva, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img{
+  width:200px;
 }
 </style>
